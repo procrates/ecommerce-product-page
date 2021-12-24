52ca17cab8a00ce7d33cc17e93be515eeeaee46d
Mikael Martinse <mikmar168@gmail.com>
2021-12-25 00:14:59 +0100

    feat: added attribution

--- app.vue
+++ app.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="relative font-kumbh-sans text-base desktop:w-desktop desktop:mx-auto">
-    <OrganismMobileNav />
-    <OrganismHeader />
-    <OrganismCart />
-    <OrganismSingleProduct />
+  <div class="relative h-screen w-screen">
+    <div
+      class="relative font-kumbh-sans text-base sm:max-w-screen-sm md:max-w-screen-md sm:mx-auto desktop:max-w-screen-desktop"
+    >
+      <OrganismMobileNav />
+      <OrganismHeader />
+      <OrganismCart />
+      <div class="desktop:max-w-screen-desktop flex">
+        <OrganismSingleProduct />
+      </div>
+      <MoleculeAttribution />
+      <div></div>
+    </div>
+    <OrganismLightboxImageSlider />
   </div>
 </template>
 <script setup lang="ts">
---
+++ components/Molecule/Attribution.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="attribution mt-20">
+        Challenge by
+        <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+        >Frontend Mentor</a>.
+        Coded by
+        <a href="#">Your Name Here</a>.
+    </div>
+</template>
+ <style>
+.attribution {
+    font-size: 11px;
+    text-align: center;
+}
+.attribution a {
+    color: hsl(228, 45%, 44%);
+}
+</style>
